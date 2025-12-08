7465b1cd1b733a3e94457e4257b14e49c794d22a
yejg <yejg1212@qq.com>
2025-12-08 15:59:58 +0800

    代码优化

--- src/main/java/com/yanglx/dubbo/test/ui/ToolBarPanel.java
+++ src/main/java/com/yanglx/dubbo/test/ui/ToolBarPanel.java
@@ -17,6 +17,8 @@ import javax.swing.JComponent;
 
 public class ToolBarPanel extends SimpleToolWindowPanel implements Disposable {
 
+    private static final float SPLIT_PROPORTION = 0.1f;
+
     private ActionManager actionManager;
 
     private TabBar tabBar;
@@ -51,35 +53,43 @@ public class ToolBarPanel extends SimpleToolWindowPanel implements Disposable {
         super(false, true);
 
         this.actionManager = ActionManager.getInstance();
+        if (this.actionManager == null) {
+            throw new IllegalStateException("Failed to obtain ActionManager instance.");
+        }
 
-        //分割线
-        JBSplitter mContentSplitter = new JBSplitter();
-        mContentSplitter.setProportion(0.1f);
-
-        //左树结构,默认为收藏
-        this.leftTree = new TreePanel(TreePanel.TreeNodeTypeEnum.COLLECTIONS);
-        this.leftTree.refresh();
-        mContentSplitter.setFirstComponent(this.leftTree);
-        //tabBar
-        this.tabBar = new TabBar(project,this.leftTree);
-        mContentSplitter.setSecondComponent(this.tabBar);
-
-        this.leftTree.setTabBar(tabBar);
-        this.setToolbar(createToolbar());
-        this.setContent(mContentSplitter);
+        buildMainLayout(project, toolWindow);
     }
 
+    private void buildMainLayout(Project project, ToolWindow toolWindow) {
+        JBSplitter contentSplitter = new JBSplitter();
+        contentSplitter.setProportion(SPLIT_PROPORTION); // 按照1:9的比例进行分割
+
+        // 左树结构，默认为收藏，占10%
+        this.leftTree = new TreePanel(TreePanel.TreeNodeTypeEnum.COLLECTIONS);
+        contentSplitter.setFirstComponent(this.leftTree);
+
+        // tabBar
+        this.tabBar = new TabBar(project, this.leftTree);
+        contentSplitter.setSecondComponent(this.tabBar);
+
+        this.leftTree.setTabBar(tabBar);
+        this.leftTree.refresh(); // 确保界面已经添加到容器之后再刷新
+
+        this.setToolbar(createToolbar());
+        this.setContent(contentSplitter);
+    }
 
     private JComponent createToolbar() {
         AddTabAction addTabAction = new AddTabAction(this.tabBar);
 
-        HistoryAbstractTabEditorAction abstractTabEditorAction = new HistoryAbstractTabEditorAction(this.leftTree);
-        CollectionsAbstractTabEditorAction collectionsAbstractTabEditorAction = new CollectionsAbstractTabEditorAction(this.leftTree);
+        HistoryAbstractTabEditorAction historyAction = new HistoryAbstractTabEditorAction(this.leftTree);
+        CollectionsAbstractTabEditorAction collectionsAction = new CollectionsAbstractTabEditorAction(this.leftTree);
         SettingAction settingAction = new SettingAction();
+
         DefaultActionGroup actionGroup = new DefaultActionGroup();
         actionGroup.add(addTabAction);
-        actionGroup.add(collectionsAbstractTabEditorAction);
-        actionGroup.add(abstractTabEditorAction);
+        actionGroup.add(collectionsAction);
+        actionGroup.add(historyAction);
         actionGroup.add(settingAction);
         ActionToolbar actionToolbar = this.actionManager.createActionToolbar("toolbar", actionGroup, false);
         actionToolbar.setTargetComponent(this.tabBar);
