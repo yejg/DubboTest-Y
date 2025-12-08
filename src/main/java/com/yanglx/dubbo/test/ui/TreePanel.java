cca9f67ed896ca9e7f2f2886bb6ae3a82e2d6e2a
yejg <yejg1212@qq.com>
2025-12-08 15:08:09 +0800

    代码优化

--- src/main/java/com/yanglx/dubbo/test/ui/TreePanel.java
+++ src/main/java/com/yanglx/dubbo/test/ui/TreePanel.java
@@ -5,20 +5,27 @@ import com.intellij.ui.tabs.TabInfo;
 import com.intellij.ui.treeStructure.Tree;
 import com.yanglx.dubbo.test.CacheInfo;
 import com.yanglx.dubbo.test.DubboSetingState;
+import com.yanglx.dubbo.test.DubboTestBundle;
 import com.yanglx.dubbo.test.dubbo.DubboMethodEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import java.awt.BorderLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 
 public class TreePanel extends JPanel {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TreePanel.class);
 
     private Tree tree;
 
@@ -26,14 +33,17 @@ public class TreePanel extends JPanel {
 
     private TabBar tabBar;
 
+    private final MouseAdapter treeMouseAdapter;
+
     public TreePanel(TreeNodeTypeEnum treeNodeTypeEnum) {
         this.nowTreeNodeTypeEnum = treeNodeTypeEnum;
         tree = new Tree();
+        this.treeMouseAdapter = createTreeMouseAdapter();
         JBScrollPane jScrollBar = new JBScrollPane(tree);
         this.setLayout(new BorderLayout());
         this.add(jScrollBar, BorderLayout.CENTER);
-        this.repaint();
-        this.validate();
+        // this.repaint();
+        // this.validate();
     }
 
     public void setTabBar(TabBar tabBar) {
@@ -54,10 +64,119 @@ public class TreePanel extends JPanel {
     public void refresh() {
         //添加数据模型
         this.setModel();
-        //添加监听
-        this.setTreeListener();
-        //添加鼠标右键事件
-        this.addTreeRightButtonAction();
+        this.resetTreeMouseListener();
+    }
+
+    /**
+     * 重置树形鼠标监听器（避免重复绑定）
+     */
+    private void resetTreeMouseListener() {
+        for (MouseListener listener : tree.getMouseListeners()) {
+            if (listener == treeMouseAdapter) {
+                tree.removeMouseListener(listener);
+            }
+        }
+        tree.addMouseListener(treeMouseAdapter);
+    }
+
+    /**
+     * 创建树形鼠标事件适配器
+     */
+    private MouseAdapter createTreeMouseAdapter() {
+        return new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    if (e.getClickCount() == 1) {
+                        handleLeftSingleClick(e);
+                    } else if (e.getClickCount() == 2) {
+                        handleLeftDoubleClick(e);
+                    }
+                } else if (SwingUtilities.isRightMouseButton(e)) {
+                    handleRightClick(e);
+                }
+            }
+        };
+    }
+
+    /**
+     * 处理左键单击事件
+     */
+    private void handleLeftSingleClick(MouseEvent e) {
+        // LOGGER.info("鼠标左键单击事件");
+    }
+
+    /**
+     * 处理左键双击事件
+     */
+    private void handleLeftDoubleClick(MouseEvent e) {
+        // LOGGER.info("鼠标左键双击事件");
+        Object selectedNodeObj = tree.getLastSelectedPathComponent();
+        if (!(selectedNodeObj instanceof DefaultMutableTreeNode) || tabBar == null) {
+            return;
+        }
+        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selectedNodeObj;
+        Object userObject = selectedNode.getUserObject();
+        if (userObject instanceof CacheInfo) {
+            CacheInfo cacheInfo = (CacheInfo) userObject;
+            tabBar.addTab(cacheInfo.getId());
+
+            TabInfo selectedTabInfo = TabBar.getSelectionTabInfo();
+            if (selectedTabInfo != null) {
+                Tab component = (Tab) selectedTabInfo.getComponent();
+                DubboMethodEntity methodEntity = cacheInfo.getDubboMethodEntity();
+                DubboPanel.refreshUI(component.getDubboPanel(), methodEntity);
+            }
+        }
+    }
+
+    /**
+     * 处理右键单击事件
+     */
+    private void handleRightClick(MouseEvent e) {
+        // LOGGER.info("鼠标右键事件");
+
+        int x = e.getX();
+        int y = e.getY();
+        TreePath pathForLocation = tree.getPathForLocation(x, y);
+        if (pathForLocation == null) {
+            return;
+        }
+        tree.setSelectionPath(pathForLocation);
+        JPopupMenu menu = createPopupMenu();
+        menu.show(tree, x, y);
+    }
+
+    private JPopupMenu createPopupMenu() {
+        JPopupMenu menu = new JPopupMenu();
+        JMenuItem menuItem = new JMenuItem(DubboTestBundle.message("dubbo-test.tool.delete"));
+        menuItem.addActionListener(actionEvent -> {
+            Object nodeObj = tree.getLastSelectedPathComponent();
+            if (!(nodeObj instanceof DefaultMutableTreeNode)) {
+                return;
+            }
+            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) nodeObj;
+            Object userObject = selectedNode.getUserObject();
+            if (userObject instanceof CacheInfo) {
+                CacheInfo cacheInfo = (CacheInfo) userObject;
+                DubboSetingState.CacheType type = TreeNodeTypeEnum.COLLECTIONS.equals(nowTreeNodeTypeEnum)
+                        ? DubboSetingState.CacheType.COLLECTIONS
+                        : DubboSetingState.CacheType.HISTORY;
+                DubboSetingState.getInstance().remove(cacheInfo, type);
+                refresh();
+            }
+        });
+        menu.add(menuItem);
+
+        if (TreeNodeTypeEnum.HISTORY.equals(nowTreeNodeTypeEnum)) {
+            JMenuItem menuItemAll = new JMenuItem(DubboTestBundle.message("dubbo-test.tool.delete-all"));
+            menuItemAll.addActionListener(actionEvent -> {
+                DubboSetingState.getInstance().historyParamInfoCacheList.clear();
+                refresh();
+            });
+            menu.add(menuItemAll);
+        }
+        return menu;
     }
 
     /**
@@ -66,13 +185,12 @@ public class TreePanel extends JPanel {
     private void setModel() {
         List<CacheInfo> paramInfoCache;
         DefaultMutableTreeNode root;
+        DubboSetingState instance = DubboSetingState.getInstance();
         if (TreeNodeTypeEnum.COLLECTIONS.equals(nowTreeNodeTypeEnum)) {
             root = new DefaultMutableTreeNode("Collections");
-            DubboSetingState instance = DubboSetingState.getInstance();
             paramInfoCache = instance.getParamInfoCache(DubboSetingState.CacheType.COLLECTIONS);
         } else {
             root = new DefaultMutableTreeNode("History");
-            DubboSetingState instance = DubboSetingState.getInstance();
             paramInfoCache = instance.getParamInfoCache(DubboSetingState.CacheType.HISTORY);
         }
         for (CacheInfo dubboMethodEntity : paramInfoCache) {
@@ -81,103 +199,6 @@ public class TreePanel extends JPanel {
         }
         tree.setModel(new DefaultTreeModel(root, false));
         tree.updateUI();
-    }
-
-    /**
-     * 添加鼠标左键点击事件
-     */
-    private void setTreeListener() {
-        //添加左测菜单点击后渲染右侧数据
-        tree.addTreeSelectionListener(e -> {
-            DefaultMutableTreeNode lastSelectedPathComponent = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-            if (lastSelectedPathComponent == null) {
-                return;
-            }
-            Object userObject = lastSelectedPathComponent.getUserObject();
-            if (userObject instanceof CacheInfo) {
-                CacheInfo cacheInfo = (CacheInfo) userObject;
-                DubboMethodEntity dubboMethodEntity = cacheInfo.getDubboMethodEntity();
-                if (TabBar.activeTabId != null && !TabBar.activeTabId.equals(cacheInfo.getId())) {
-                    TabInfo tabInfo = tabBar.getTabInfo(cacheInfo.getId());
-                    if (tabInfo != null) {
-                        tabBar.select(tabInfo, false); // 切换tab
-                    }
-                }
-                // TabInfo selectedInfo = TabBar.getSelectionTabInfo();
-                // if (selectedInfo != null) {
-                //     Tab component = (Tab) selectedInfo.getComponent();
-                //     DubboPanel.refreshUI(component.getDubboPanel(), dubboMethodEntity);
-                // }
-            }
-        });
-    }
-
-    /**
-     * 添加鼠标右键事件
-     */
-    private void addTreeRightButtonAction() {
-        //右键删除操作
-        JPopupMenu menu = new JPopupMenu();
-        JMenuItem menuItem = new JMenuItem("Delete");
-        menu.add(menuItem);
-        if (TreeNodeTypeEnum.HISTORY.equals(nowTreeNodeTypeEnum)) {
-            JMenuItem menuItemAll = new JMenuItem("Delete all");
-            menu.add(menuItemAll);
-            menuItemAll.addActionListener(e -> {
-                //删除所有
-                DubboSetingState.getInstance().historyParamInfoCacheList.clear();
-                this.refresh();
-            });
-        }
-        menuItem.addActionListener(e -> {
-            DefaultMutableTreeNode lastSelectedPathComponent = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-            if (lastSelectedPathComponent == null) {
-                return;
-            }
-            Object userObject = lastSelectedPathComponent.getUserObject();
-            if (userObject instanceof CacheInfo) {
-                CacheInfo cacheInfo = (CacheInfo) userObject;
-                if (TreeNodeTypeEnum.COLLECTIONS.equals(this.nowTreeNodeTypeEnum)) {
-                    DubboSetingState.getInstance().remove(cacheInfo, DubboSetingState.CacheType.COLLECTIONS);
-                } else {
-                    DubboSetingState.getInstance().remove(cacheInfo, DubboSetingState.CacheType.HISTORY);
-                }
-                this.refresh();
-            }
-        });
-        //有限显示删除popup
-        this.tree.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                if (e.getClickCount() == 2) {
-                    // 双击新增 tab
-                    DefaultMutableTreeNode lastSelectedPathComponent = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-                    if (lastSelectedPathComponent == null) {
-                        return;
-                    }
-                    Object userObject = lastSelectedPathComponent.getUserObject();
-                    if (userObject instanceof CacheInfo) {
-                        CacheInfo cacheInfo = (CacheInfo) userObject;
-                        // tabBar.addTab("" + e.getID() + e.getWhen());
-                        tabBar.addTab(cacheInfo.getId());
-                        Tab component = (Tab) TabBar.getSelectionTabInfo().getComponent();
-                        DubboMethodEntity dubboMethodEntity = ((CacheInfo) userObject).getDubboMethodEntity();
-                        DubboPanel.refreshUI(component.getDubboPanel(), dubboMethodEntity);
-                    }
-                    return;
-                }
-
-                int x = e.getX();
-                int y = e.getY();
-                if (e.getButton() == MouseEvent.BUTTON3) {
-                    TreePath pathForLocation = tree.getPathForLocation(x, y);
-                    tree.setSelectionPath(pathForLocation);
-                    menu.show(tree, x, y);
-                }
-            }
-        });
-
     }
 
     public enum TreeNodeTypeEnum {
